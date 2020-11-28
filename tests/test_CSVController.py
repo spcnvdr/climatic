501860ad1a1d2e3b76e559df4c94f5551287b95c
GHZB <zebulon.e.burroughs.temp@gmail.com>
2020-11-27 19:19:57 -0600

    csv upload convert to json with testing

--- tests/test_CSVController.py
+++ tests/test_CSVController.py
@@ -4,4 +4,4 @@ def test_upload_csv(www):
         'csv_file': (open(file, 'rb'), file)
     }
     response = www.post('/upload/csv', data=data)
-    assert response.status_code == 302
+    assert response.status_code == 200
